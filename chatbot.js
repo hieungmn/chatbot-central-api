a8f4eee5de13c069cad90d04f9a0b1b1d1147971
hieungmn <nguyenminhhieu050602@gmail.com>
2026-05-26 17:27:19 +0900

    chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -56,7 +56,7 @@
         closeBtn.addEventListener('click', () => { box.style.display = 'none'; });
 
         // 3. TỰ ĐỘNG PHÁT HIỆN LINK API (Nếu chạy localhost dùng cổng 3000, nếu chạy online tự lấy link Render)
-        let baseUrl = "https://chatbot-central-api.onrender.com/api/v1/chatbot/query";
+        let baseUrl = "http://localhost:3000";
         const currentScript = document.querySelector('script[src*="chatbot-widget.js"]');
         if (currentScript) {
             const src = currentScript.getAttribute('src');
